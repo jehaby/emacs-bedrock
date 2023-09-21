33e76faf2d43f3dbb2b94a797a3c86d292820772
Stas Makarov <stas.makarov.90@gmail.com>
2023-09-21 14:56:42 +0400

    evil leader, global map with yammy stuff (thx @sheepduke)

--- extras/vim-like.el
+++ extras/vim-like.el
@@ -14,8 +14,6 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; required by evil-mode
-(setq evil-want-keybinding nil)
 
 ;; Evil: vi emulation
 (use-package evil
@@ -24,15 +22,83 @@
   :init
   (setq evil-respect-visual-line-mode t)
   (setq evil-undo-system 'undo-redo)
+  (setq evil-want-keybinding nil)
 
   ;; Enable this if you want C-u to scroll up, more like pure Vim
   ;(setq evil-want-C-u-scroll t)
+
+  (define-prefix-command 'evil-leader-map)
 
   :config
   (evil-mode)
 
   ;; Configuring initial major mode for some modes
-  (evil-set-initial-state 'vterm-mode 'emacs))
+  (evil-set-initial-state 'vterm-mode 'emacs)
+
+  (evil-set-leader '(motion normal) (kbd "SPC"))
+  (evil-set-leader '(motion normal) (kbd ",") 'local)
+
+  :bind
+  (:map evil-leader-map
+        ;; Buffer - B
+        ("b q" . #'bury-buffer)
+        ("b s" . #'save-buffer)
+        ("b b" . #'switch-to-buffer)
+        ("b o" . #'switch-to-other-buffer)
+        ("b r" . #'consult-bookmark)
+        ("b l" . #'consult-buffer)
+        ("b p" . #'consult-projectile)
+        ("b d" . #'dired)
+        ("b f" . #'find-file)
+        ("b k" . #'kill-this-buffer)
+
+        ;; Editing - E
+        ;; ("e c" . #'copy-whole-buffer)
+        
+        ;; Goto - G
+        ("g l" . #'avy-goto-line)
+        ("g s" . #'consult-line)
+        ("g m" . #'consult-mark)
+        ("g o" . #'consult-outline)
+
+        ;; Help - H
+        ("h f" . #'describe-function)
+        ("h k" . #'describe-key)
+        ("h m" . #'describe-mode)
+        ("h s" . #'describe-symbol)
+        ("h v" . #'describe-variable)
+        ("h i" . #'info)
+
+        ;; ;; Org - O
+        ;; ("o a" . #'org-agenda)
+        ;; ("o c" . #'org-capture)
+        ;; ("o o" . #'org-journal-open-current-journal-file)
+        ;; ("o j" . #'org-journal-new-entry)
+
+        ;; Perspecitve - P
+        ;; ("p" . #'perspective-map)
+
+        ;; Tools - T
+        ("t f" . #'auto-fill-mode)
+        ("t c" . #'calendar)
+        ;; ("t e" . #'eshell)
+        ;; ("t l" . #'linum-mode)
+        ;; ("t m" . #'notmuch)
+        ("t t" . #'toggle-truncate-lines)
+        ("t w" . #'toggle-word-wrap)
+      ;;  ("t u" . #'undo-tree-visualize)
+        )
+  
+  ;; (:map evil-window-map
+  ;;       ("C-d" . #'toggle-window-dedicated)
+  ;;       ("C-w" . #'ace-window)
+  ;;       ("M-w" . #'ace-swap-window)
+  ;;       ("u" . #'winner-undo))
+
+  (:map evil-normal-state-map
+        ("SPC" . evil-leader-map))
+
+  )
 
 ;; vi keys support for other mods
 (use-package evil-collection
