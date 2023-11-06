41592131ee122c34e27a21815e47eb62f5b01ad7
Stas Makarov <stas.makarov.90@gmail.com>
2023-11-06 17:10:04 +0400

    vanilla gc-cons-threshold

--- early-init.el
+++ early-init.el
@@ -16,7 +16,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Startup speed, annoyance suppression
-(setq gc-cons-threshold 10000000)
 (setq byte-compile-warnings '(not obsolete))
 (setq warning-suppress-log-types '((comp) (bytecomp)))
 (setq native-comp-async-report-warnings-errors 'silent)
